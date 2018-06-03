81f2d4c6ac0b9bcfd20a053c2db94ba6fb994260
mareaokim <39861011+mareaokim@users.noreply.github.com>
2018-06-04 00:55:18 +0900

    Add files via upload

---
+++ 01chaos.py
@@ -0,0 +1,91 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 17:00:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+>>> def main():
+	print("This program illustrates a chaotic function")
+	x=eval(input("Enter a number between 0 and 1 : "))
+	for  i  in range(10):
+		x= 3.9 * x * (1-x)
+		print(x)
+
+		
+>>> main()
+This program illustrates a chaotic function
+Enter a number between 0 and 1 : 0.25
+0.73125
+0.76644140625
+0.6981350104385375
+0.8218958187902304
+0.5708940191969317
+0.9553987483642099
+0.166186721954413
+0.5404179120617926
+0.9686289302998042
+0.11850901017563877
+>>> 
+def main():
+	print("This program illustrates a chaotic function")
+	x=eval(input("Enter a number between 0 and 1 : "))
+	for  i  in range(10):
+		x= 3.9 * x * (1-x)
+		print(x)
+
+		
+>>> main()
+This program illustrates a chaotic function
+Enter a number between 0 and 1 : 0.26
+0.75036
+0.73054749456
+0.7677066257332165
+0.6954993339002887
+0.8259420407337192
+0.5606709657211202
+0.9606442322820199
+0.14744687593470315
+0.49025454937601765
+0.9746296021493285
+>>> 
+def main():
+	print("This program illustrates a chaotic function")
+	x=eval(input("Enter a number between 0 and 1 : "))
+	for  i  in range(10):
+		x= 3.9 * x * (1-x)
+		print(x)
+
+		
+>>> main()
+This program illustrates a chaotic function
+Enter a number between 0 and 1 : .25
+0.73125
+0.76644140625
+0.6981350104385375
+0.8218958187902304
+0.5708940191969317
+0.9553987483642099
+0.166186721954413
+0.5404179120617926
+0.9686289302998042
+0.11850901017563877
+>>> 
+>>> def main():
+	print("This program illustrates a chaotic function")
+	x=eval(input("Enter a number between 0 and 1 : "))
+	for  i  in range(10):
+		x= 3.9 * x * (1-x)
+		print(x)
+
+		
+>>> main()
+This program illustrates a chaotic function
+Enter a number between 0 and 1 : .26
+0.75036
+0.73054749456
+0.7677066257332165
+0.6954993339002887
+0.8259420407337192
+0.5606709657211202
+0.9606442322820199
+0.14744687593470315
+0.49025454937601765
+0.9746296021493285
+>>> # 실수로 0.25 ,0.26찍어서 다시 했습니다.
