e44da4df3015ee4bd90d28923e1e3a2499f217d1
mareaokim <39861011+mareaokim@users.noreply.github.com>
2018-06-24 22:27:45 +0900

    Add files via upload

---
+++ _02_11번change.py.py
@@ -0,0 +1,11 @@
+
+def main():
+    print("This program converts Kilogram to Gram.")
+
+
+    kg = eval(input("Enter the distance in Kilogram : "))
+    g = kg * 1000
+    print("The distance is", g ,"gram.")
+
+
+main()
