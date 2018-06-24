b20f841e4fe4338b873939a59f98223165ab3574
mareaokim <39861011+mareaokim@users.noreply.github.com>
2018-06-24 22:08:49 +0900

    Add files via upload

---
+++ _02_10번Kilometers.py.py
@@ -0,0 +1,11 @@
+
+def main():
+    print("This program converts kilometers to miles.")
+
+
+    kms = eval(input("Enter the distance in kilometers : "))
+    miles = kms * 0.62
+    print("The distance is", miles ,"miles.")
+
+
+main()
