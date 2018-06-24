3fe6c019a3bb0799517093d42aae5d7673ad7884
mareaokim <39861011+mareaokim@users.noreply.github.com>
2018-06-24 22:03:48 +0900

    Add files via upload

---
+++ _02_9번Fahrenheit.py.py
@@ -0,0 +1,12 @@
+
+def main():
+    print("The program converts temperratures from Fahrenheit to Celsius")
+    print()
+
+
+    fahrenheit = eval(input("What is the Fahrenheit temperature?"))
+    celsius = 5.0 / 9.0 * (fahrenheit - 32)
+    print("The temperature is" , celsius , "degrees Celsius") 
+
+
+main() 
