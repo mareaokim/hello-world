2ee563ebbf6ea670b2ea0c75c49d2ae29caa2273
mareaokim <39861011+mareaokim@users.noreply.github.com>
2018-06-24 17:24:12 +0900

    Update 02_4번Convert.py.py

--- 02_4번Convert.py.py
+++ 02_4번Convert.py.py
@@ -8,3 +8,16 @@ def main():
 
 
 main()
+
+
+what is the celsius temperature?10
+The temperature is 50.0 dgrees Fahrenheit.
+what is the celsius temperature?69
+The temperature is 156.2 dgrees Fahrenheit.
+what is the celsius temperature?1
+The temperature is 33.8 dgrees Fahrenheit.
+what is the celsius temperature?6969
+The temperature is 12576.2 dgrees Fahrenheit.
+what is the celsius temperature?69696969
+The temperature is 125454576.2 dgrees Fahrenheit.
+Press any key to continue . . .
