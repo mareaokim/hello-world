2f4399dfdbd286c91addf93c2a667cf184bfad40
mareaokim <39861011+mareaokim@users.noreply.github.com>
2018-06-24 17:04:16 +0900

    Add files via upload

---
+++ PythonApplication4.py
@@ -0,0 +1,11 @@
+# 3번 문제
+
+def main():
+    print("This program computes the average of two exam scores.")
+    score1, score2, score3 = eval(input("Enter three scores separated by a comma : "))
+    average = (score1 + score2 + score3) / 3
+    print("The average of the score is :" , average)
+
+main()
+
+
