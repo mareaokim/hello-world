e8b491c529081e33391e5e6c8fed62641be42ff0
mareaokim <39861011+mareaokim@users.noreply.github.com>
2018-06-24 21:57:19 +0900

    Update and rename _02_8번Future.py.py to 02_8번Future.py

--- 02_8번Future.py
+++ 02_8번Future.py
@@ -18,3 +18,19 @@ def main():
 
         
 main()
+
+>>>>
+
+This program calculates the future value
+
+Enter the initial principal : 5
+Enter the annual interest rate: 2
+Enter the number of compounding periods per year:6
+Enter the number of years: 1
+The amount in 1 years is: 6.666666666666666
+The amount in 1 years is: 8.888888888888888
+The amount in 1 years is: 11.85185185185185
+The amount in 1 years is: 15.802469135802465
+The amount in 1 years is: 21.06995884773662
+The amount in 1 years is: 28.093278463648822
+Press any key to continue . . .
