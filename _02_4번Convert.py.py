c00235d4d6e2bbbdac395737724b95e4caf459b7
mareaokim <39861011+mareaokim@users.noreply.github.com>
2018-06-24 17:21:52 +0900

    Add files via upload
    what is the celsius temperature?10
    The temperature is 50.0 dgrees Fahrenheit.
    what is the celsius temperature?69
    The temperature is 156.2 dgrees Fahrenheit.
    what is the celsius temperature?1
    The temperature is 33.8 dgrees Fahrenheit.
    what is the celsius temperature?6969
    The temperature is 12576.2 dgrees Fahrenheit.
    what is the celsius temperature?69696969
    The temperature is 125454576.2 dgrees Fahrenheit.
    Press any key to continue . . .

---
+++ _02_4번Convert.py.py
@@ -0,0 +1,10 @@
+def main():
+    for a in range(5):
+        celsius = eval(input("what is the celsius temperature?"))
+     
+        fahrenheit = 9/5*celsius+32
+     
+        print("The temperature is", fahrenheit, "dgrees Fahrenheit.")
+
+
+main()
