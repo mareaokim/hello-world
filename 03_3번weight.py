4afbbbcb0312df1b5eda915698470955a665c992
mareaokim <39861011+mareaokim@users.noreply.github.com>
2018-07-02 22:44:27 +0900

    Update and rename _03_3번weight.py to 03_3번weight.py

--- 03_3번weight.py
+++ 03_3번weight.py
@@ -15,3 +15,15 @@ def main():
 main()
 
 
+
+>>>
+
+This program calculates the molecular weight of a hydrocarbon.
+
+Enter the number of hydrogen atoms : 45
+Enter the number of carbon atoms : 56
+Enter the number of oxygen atoms : 55
+
+The molecular weight is : 1597.9234999999999
+Press any key to continue . . .
+
