bbf385e705c21c82e09e8467d9212b14847d820d
mareaokim <39861011+mareaokim@users.noreply.github.com>
2018-07-03 00:31:54 +0900

    Update and rename _03_12번number2.py to 03_12번number2.py

---
+++ 03_12번number2.py
@@ -0,0 +1,40 @@
+def main():
+    print("This program finds the sum of the cubes of the first n natural numbers.")
+    print()
+
+    n = int(input("Please enter a value for n : "))
+    sum = 0
+    for i in range(1,n+1):
+        sum = sum + i**3
+
+        print()
+        print("The sum of cubes of 1 through through", n, "is : ",sum)
+
+main()
+
+
+>>>>>>>>>>>>>>>>
+
+This program finds the sum of the cubes of the first n natural numbers.
+
+Please enter a value for n : 9
+
+The sum of cubes of 1 through through 9 is :  1
+
+The sum of cubes of 1 through through 9 is :  9
+
+The sum of cubes of 1 through through 9 is :  36
+
+The sum of cubes of 1 through through 9 is :  100
+
+The sum of cubes of 1 through through 9 is :  225
+
+The sum of cubes of 1 through through 9 is :  441
+
+The sum of cubes of 1 through through 9 is :  784
+
+The sum of cubes of 1 through through 9 is :  1296
+
+The sum of cubes of 1 through through 9 is :  2025
+Press any key to continue . . .
+
