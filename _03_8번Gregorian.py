b8854bc9d8152f883e15a4fe55329cd47087a510
mareaokim <39861011+mareaokim@users.noreply.github.com>
2018-07-02 23:42:07 +0900

    Add files via upload

---
+++ _03_8번Gregorian.py
@@ -0,0 +1,12 @@
+def main(): 
+    print("Gregorian epact value of year.")
+    print()
+
+    year = int(input("Enter the year (e.g. 2020) : "))
+    c = year // 100
+    epact = (8+(c//4) - c + ((8*c+13)//25) + 11 * (year % 19)) %  30
+    
+    print()
+    print("The epact value is" , epact, "days.")
+
+main()
