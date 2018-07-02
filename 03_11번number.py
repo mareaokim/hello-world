d4759c90531acf101b2cf113c92308e9aaf83715
mareaokim <39861011+mareaokim@users.noreply.github.com>
2018-07-03 00:25:20 +0900

    Update and rename _03_11번number.py to 03_11번number.py

---
+++ 03_11번number.py
@@ -0,0 +1,31 @@
+def main():
+    print("natural numbers.")
+    print()
+
+    n = int(input("Please enter a value for n : "))
+    sum = 0
+    for i in range(1,n+1):
+        sum = sum + i
+
+        print()
+        print("The sum from 1 to",n,"is:",sum )
+
+main()
+
+
+>>>>>>>>>>>>>>>>>>>>>
+
+natural numbers.
+
+Please enter a value for n : 5
+
+The sum from 1 to 5 is: 1
+
+The sum from 1 to 5 is: 3
+
+The sum from 1 to 5 is: 6
+
+The sum from 1 to 5 is: 10
+
+The sum from 1 to 5 is: 15
+Press any key to continue . . .
