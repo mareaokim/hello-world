3b380b92f85486a8fc98aaf92c8893912383345f
mareaokim <39861011+mareaokim@users.noreply.github.com>
2018-07-03 00:01:30 +0900

    Update and rename _03_9번triangle.py to 03_9번triangle.py

--- 03_9번triangle.py
+++ 03_9번triangle.py
@@ -15,3 +15,14 @@ def main():
     print("The area is" , area, "square units.")
 
 main()
+
+>>>>>>>>>>>
+
+This program calculates the are of a triangle.
+
+Enter the length of side a : 3.6
+Enter the length of side b : 5.9
+Enter the length of side c : 9
+
+The area is 6.615878154107738 square units.
+Press any key to continue . . .
