0be0653b4e1b9b1d7e5046dee50512a10e92338d
mareaokim <39861011+mareaokim@users.noreply.github.com>
2018-07-03 00:44:15 +0900

    Update and rename _03_14average.py to 03_14average.py

--- 03_14average.py
+++ 03_14average.py
@@ -12,3 +12,20 @@ def main():
         print("The average of the numbers is:", total/n)
 
 main()
+
+>>>>>>>>>>>>>>>>>>>>>>
+
+average
+
+How many numbers do you have? 8
+Enter a number: 66
+
+The average of the numbers is: 8.25
+Enter a number: 9
+
+The average of the numbers is: 9.375
+Enter a number: 44
+
+The average of the numbers is: 14.875
+Enter a number:
+
