e127183c64efdf926d58d7c735579aab8cf83429
mareaokim <39861011+mareaokim@users.noreply.github.com>
2018-07-03 01:08:37 +0900

    Update _03_17번newton.py

--- _03_17번newton.py
+++ _03_17번newton.py
@@ -18,3 +18,24 @@ def main():
 
 main()
 
+>>>>>>>>>>>>>>>>>>
+This is program calculates square root using Newton's method.
+
+Enter number to find the root of:98
+How many iterations should i use?5
+
+Approximate square root: 25.5
+Difference from math.sqrt: -15.600505063388335
+
+Approximate square root: 14.67156862745098
+Difference from math.sqrt: -4.772073690839315
+
+Approximate square root: 10.675577163708784
+Difference from math.sqrt: -0.7760822270971186
+
+Approximate square root: 9.927704353956496
+Difference from math.sqrt: -0.028209417344831067
+
+Approximate square root: 9.899535014921744
+Difference from math.sqrt: -4.00783100786839e-05
+Press any key to continue . . .
