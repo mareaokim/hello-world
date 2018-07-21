54f7690dd57c27d19d7e8fbaeda2715c8f66f0fd
mareaokim <39861011+mareaokim@users.noreply.github.com>
2018-07-22 00:16:04 +0900

    Add files via upload

---
+++ c04ex01.py
@@ -0,0 +1,24 @@
+# c04ex01.pyw
+#   Drawing with square bricks
+
+from graphics import *
+
+def main():
+    win = GraphWin()
+    shape = Rectangle(Point(40,40), Point(60,60))
+    shape.setOutline("red")
+    shape.setFill("red")
+    shape.draw(win)
+    for i in range(10):
+        p = win.getMouse()
+        c = shape.getCenter()
+        dx = p.getX() - c.getX()
+        dy = p.getY() - c.getY()
+        shape = shape.clone()
+        shape.move(dx,dy)
+        shape.draw(win)
+    Text(Point(100,180), "Click again to quit.").draw(win)
+    win.getMouse()
+    win.close()
+
+main()
