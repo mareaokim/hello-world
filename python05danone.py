39b1712dddebfcb4c3b546c2a728df5e89ff08a1
mareaokim <39861011+mareaokim@users.noreply.github.com>
2018-07-30 00:14:05 +0900

    Add files via upload

---
+++ python05danone.py
@@ -0,0 +1,18 @@
+
+def main():
+    day = int(input("Day number 를 입력하시오 : "))
+    month =int(input("month number 를 입력하시오 : ")) 
+    year = int(input("year number 를 입력하시오 : ")) 
+
+
+
+    date1 = "{0}/{1}/{2}".format(month,day,year)
+
+    months =  ["January", "February", "March", "April", "May", "June",
+              "July", "August", "September", "October", "November", "December"]
+    date2 = "{0} {1} {2}".format(months[month-1],day,year)
+
+    print("The date is {0} or {1}.".format(date1,date2))
+
+
+main()
